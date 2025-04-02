1bcaa88fff3fe35a1b44c26455022688a1111f82
J1manuel <jmanuelvergara.22@gmail.com>
2025-04-02 16:18:30 -0500

    user

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@email.com', 'admin123')
+" | python manage.py shell
